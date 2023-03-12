fcb0b80e2df9eb4f0c8d2854e76b5ab1bdb8696c
“joseesco24” <josed.escobar@outlook.com>
2023-03-11 21:02:39 -0500

    build: unused dependencies removed

--- src/main.ts
+++ src/main.ts
@@ -5,9 +5,6 @@ import helmet from "helmet"
 
 // ** info: nestjs imports
 import { INestApplication } from "@nestjs/common"
-import { DocumentBuilder } from "@nestjs/swagger"
-import { SwaggerModule } from "@nestjs/swagger"
-import { OpenAPIObject } from "@nestjs/swagger"
 import { ValidationPipe } from "@nestjs/common"
 import { NestFactory } from "@nestjs/core"
 
@@ -15,12 +12,14 @@ import { NestFactory } from "@nestjs/core"
 import { AppModule } from "./app.module"
 
 async function main(): Promise<void> {
-	// Setting up the main server.
-
+	// ---------------------------------------------------------------------------------------------------------------------
+	// ** info: setting up the main server
+	// ---------------------------------------------------------------------------------------------------------------------
 	const app: INestApplication = await NestFactory.create(AppModule)
 
-	// Setting up the main server global pipes.
-
+	// ---------------------------------------------------------------------------------------------------------------------
+	// ** info: setting up the main server global pipes
+	// ---------------------------------------------------------------------------------------------------------------------
 	app.useGlobalPipes(
 		new ValidationPipe({
 			forbidNonWhitelisted: true,
@@ -29,21 +28,15 @@ async function main(): Promise<void> {
 		})
 	)
 
-	// Setting up the main server middlewares.
-
+	// ---------------------------------------------------------------------------------------------------------------------
+	// ** info: setting up the main server middlewares
+	// ---------------------------------------------------------------------------------------------------------------------
 	app.use(helmet())
 	app.enableCors()
 
-	// Setting up swagger documentation.
-
-	if (process.env.ENVIRONMENT_MODE === "development") {
-		const config: Omit<OpenAPIObject, "paths"> = new DocumentBuilder().build()
-		const document: OpenAPIObject = SwaggerModule.createDocument(app, config)
-		SwaggerModule.setup("apispec", app, document)
-	}
-
-	// Starting the main server.
-
+	// ---------------------------------------------------------------------------------------------------------------------
+	// ** info: starting the main server
+	// ---------------------------------------------------------------------------------------------------------------------
 	await app.listen(Number(process.env.PORT as string) || 3000)
 }
 
