9fbbf65f62e68158a7833b50dae1ecb5550ec9b8
“joseesco24” <josed.escobar@outlook.com>
2023-06-23 23:53:13 -0500

    feat: improvements on githooks scripts

---
+++ .husky/commit-msg.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+. "$(dirname "$0")/_/husky.sh"
+
+print_title() {
+    TIT='\033[1;33m'
+    NCL='\033[0m'
+    title=$1
+    set -e
+    echo -e
+    echo -e "${TIT}${title}${NCL}"
+    echo -e
+}
+
+print_title "Linting Commit"
+
+npx commitlint --edit
